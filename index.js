8d3162335e23d61bb999882a61131ee4e8ab4ee2
Paras Mahajan <parasmahajan1010@gmail.com>
2025-09-20 20:27:43 +0530

    DID SOME REFACTORING AND I THINK I NEED TO DO IT AGAIN AFTER CREATING BRANCH

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 console.log("It's working");
 
+function askingInput() {
+    let inputGrid = prompt("How many squares you want in one line?");
+    if (inputGrid > 100) {askingInput()};
+    return inputGrid;
+};
+const button = document.createElement("button");
+document.body.appendChild(button);
+button.addEventListener("click", eventButton);
+
+button.textContent = "Clear and start again";
+
+function eventButton() { 
+
 const block =  document.createElement("div");
 document.body.appendChild(block);
 block.setAttribute("class", "main-block");
 
-for (let i = 1; i <= 256; i++) {
+let value = askingInput();
+
+for (let i = 1; i <= value; i++) {
     let div = document.createElement("div");
     div.className = "box";
 
     block.appendChild(div);
+};
+
+function randomColor() {
+    // const number = Math.floor(Math.random()*4);
+    let bgColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random()*256)}, 50)`;
+    return bgColor
+}
+
+document.body.addEventListener("mouseover", (event) => {
+    // console.log(event.target);
+    // event.target.setAttribute('class', "randomColor");
+    // event.target.setAttribute("style", `background-color:${randomColor()}`)
+    // console.log(event.target.closest("div"))
+    let effectedArea = event.target.closest("div");
+    if (!effectedArea) {return ;}
+    effectedArea.style.backgroundColor = randomColor();
+})
+
+document.body.addEventListener("mouseout", (event) => {
+    let effectedArea = event.target.closest("div");
+    console.log(effectedArea);
+    if (!effectedArea) {return ;}
+    effectedArea.style.backgroundColor = "";
+})
+// button.removeEventListener("click", eventButton);
 }
